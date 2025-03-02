bb2c890017f989023a63975c56c7b45e2632c7d0
Randi Weston <rweston2@illinois.edu>
2025-03-02 00:52:29 -0800

    added styling, video, and tilt/pan controls

--- FrontEnd/electron/index.js
+++ FrontEnd/electron/index.js
@@ -1,13 +1,15 @@
 document.onkeydown = updateKey;
 document.onkeyup = resetKey;
+document.onmouseup = resetKey;
 
 var server_port = 65432;
 var server_addr = "10.0.0.207";   // the IP address of your Raspberry PI
 
-function client(){
+function client(input){
     
     const net = require('net');
-    var input = document.getElementById("message").value;
+    //var input = document.getElementById("message").value;
+    console.log(input)
 
     const client = net.createConnection({ port: server_port, host: server_addr }, () => {
         // 'connect' listener.
@@ -33,28 +35,58 @@ function client(){
 
 // for detecting which key is been pressed w,a,s,d
 function updateKey(e) {
+    console.log("here")
+    console.log(e)
 
     e = e || window.event;
 
     if (e.keyCode == '87') {
         // up (w)
-        document.getElementById("upArrow").style.color = "green";
-        send_data("87");
+        document.getElementById("upArrow").style.borderBottom = "50px solid green";
+        //send_data("87");
+        client('w')
     }
     else if (e.keyCode == '83') {
         // down (s)
-        document.getElementById("downArrow").style.color = "green";
-        send_data("83");
+        document.getElementById("downArrow").style.borderTop = "50px solid green";
+        //send_data("83");
+        client('s')
     }
     else if (e.keyCode == '65') {
         // left (a)
-        document.getElementById("leftArrow").style.color = "green";
-        send_data("65");
+        document.getElementById("leftArrow").style.borderRight = "50px solid green";
+        //send_data("65");
+        client('a')
     }
     else if (e.keyCode == '68') {
         // right (d)
-        document.getElementById("rightArrow").style.color = "green";
-        send_data("68");
+        document.getElementById("rightArrow").style.borderLeft = "50px solid green";
+        //send_data("68");
+        client('d')
+    }
+    else if (e.keyCode == '73') {
+        // head up (i)
+        document.getElementById("tiltUpArrow").style.borderBottom = "50px solid green";
+        //send_data("68");
+        client('i')
+    }
+    else if (e.keyCode == '75') {
+        // head down (k)
+        document.getElementById("tiltDownArrow").style.borderTop = "50px solid green";
+        //send_data("68");
+        client('k')
+    }
+    else if (e.keyCode == '74') {
+        // head left (j)
+        document.getElementById("panLeftArrow").style.borderRight = "50px solid green";
+        //send_data("68");
+        client('j')
+    }
+    else if (e.keyCode == '76') {
+        // head right (l)
+        document.getElementById("panRightArrow").style.borderLeft = "50px solid green";
+        //send_data("68");
+        client('l')
     }
 }
 
@@ -63,10 +95,14 @@ function resetKey(e) {
 
     e = e || window.event;
 
-    document.getElementById("upArrow").style.color = "grey";
-    document.getElementById("downArrow").style.color = "grey";
-    document.getElementById("leftArrow").style.color = "grey";
-    document.getElementById("rightArrow").style.color = "grey";
+    document.getElementById("upArrow").style.borderBottom = "50px solid grey";
+    document.getElementById("downArrow").style.borderTop = "50px solid grey";
+    document.getElementById("leftArrow").style.borderRight = "50px solid grey";
+    document.getElementById("rightArrow").style.borderLeft = "50px solid grey";
+    document.getElementById("tiltUpArrow").style.borderBottom = "50px solid grey";
+    document.getElementById("tiltDownArrow").style.borderTop = "50px solid grey";
+    document.getElementById("panLeftArrow").style.borderRight = "50px solid grey";
+    document.getElementById("panRightArrow").style.borderLeft = "50px solid grey";
 }
 
 
